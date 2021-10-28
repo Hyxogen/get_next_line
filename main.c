71ee82021f179a3bbda1639d9a66986f045e45f2
Daan Meijer <daansander02@outlook.com>
2021-10-28 09:40:31 +0200

    Add newline

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dmeijer <dmeijer@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/27 17:25:58 by dmeijer       #+#    #+#                 */
-/*   Updated: 2021/10/27 17:32:01 by dmeijer       ########   odam.nl         */
+/*   Updated: 2021/10/28 09:38:03 by dmeijer       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int
 	while (1)
 	{
 	    line = get_next_line(fd);
-        printf("RESULT:\"%s\"", line);
+        printf("RESULT:\"%s\"\n", line);
         if (!line)
             break ;
         free(line);
