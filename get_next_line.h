c98711133ce5a196fb84de4e2c87f0daa18597f7
Daan Meijer <daansander02@outlook.com>
2021-11-09 10:45:34 +0100

    Clean up header

--- get_next_line.h
+++ get_next_line.h
@@ -16,18 +16,17 @@
 # include <wchar.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1
+#  define BUFFER_SIZE 128
 # endif
 
-char	*ft_strndup(const char *str, size_t len);
+# define TRUE 1
+# define FALSE 0
 
 void	*ft_memchr(const void *arr, unsigned char c, size_t n);
 
 void	*ft_memcpy(void *dst, const void *src, size_t len);
 
 void	*ft_realloc(void *src, size_t old_len, size_t new_len);
-
-char	*ft_strjoin(const char *str1, const char *str2);
 
 void	*ft_memset(void *src, int c, size_t len);
 
@@ -39,8 +38,4 @@ typedef struct s_line_buffer {
 	char	*m_End;
 	ssize_t	m_LastRead;
 }	t_line_buffer;
-
-# define TRUE 1
-# define FALSE 0
-
 #endif
